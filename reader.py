d165fcdd87b5ffb682699a46ca1685ec80d56a73
crystalpal <joren.verspeelt@gmail.com>
2016-04-12 17:26:04 +0200

    python stuffs

--- reader.py
+++ reader.py
@@ -13,8 +13,11 @@ import time as tm
 
 
 file = 'C:/Users/Joren/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test.csv'
-file1 = 'C:/Users/Joren/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test/march_22.csv'
+file1 = 'F:/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test/march_13.csv'
 
+colors = ["red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold", 
+          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"          
+          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"]  
     
 class Action(object):
     action =""
@@ -30,17 +33,20 @@ class Action(object):
         
     def timedifference(self, action):
         time = tm.mktime(action.timestamp) - tm.mktime(self.timestamp)
-        if time < 0:
-            print(self.timestamp)
-            print(action.timestamp)
-        return tm.mktime(action.timestamp) - tm.mktime(self.timestamp) 
+        return tm.mktime(action.timestamp) - tm.mktime(self.timestamp)
+        
+class Domain(object):
+    url = ""
+    color = ""
+    def __init__(self, url, color):
+        self.dom = url
+        self.color = color
     
 def line_prepender(filename, line):
     with open(filename, 'r+') as f:
         content = f.read()
         content = content.replace(", ", ",")
         content = content.replace('"', "")
-        print (content)
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') +  '\n' + content)
         
@@ -54,6 +60,8 @@ data = csv.reader(f, delimiter=',')
 
 loads = []
 clicks = []
+doms = []
+d = []
 
 
 iterrows = iter(data)
@@ -79,58 +87,61 @@ for row in iterrows:
     if act == "load":
         loads.append(action)
     if act == "click":
-        print(action.link)
         clicks.append(action)
+    if not action.domain in d:
+        dom = Domain(action.domain, colors[len(doms)])
+        doms.append(dom)
+        d.append(action.domain)
    
 nodes = [] 
 domains = []
+domainedges = []
 maxtime = 0  
 for i in range(0, len(clicks)-1):
-     nodes.append((clicks[i], clicks[i+1])) 
-     time = clicks[i].timedifference(clicks[i+1])
-     if time > maxtime:
-         maxtime = time
-        
+    nodes.append((clicks[i], clicks[i+1])) 
+    time = clicks[i].timedifference(clicks[i+1])
+    if time > maxtime:
+        maxtime = time
+   
+for i in range(0, len(doms)-1):
+    domains.append((doms[i], doms[i+1]))
+     
+         
 edges = []
 for node in nodes:
     time = node[0].timedifference(node[1])
-    edges.append((node[0], node[1], time))
+    edges.append((node[0], node[1], time/maxtime))
 
-for click in clicks:
-    if not click.domain in domains:
-        domains.append(click.domain)   
- 
-colors = ["red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold", 
-          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"          
-          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"]  
+
 colormapping = dict(zip(domains, colors))
 
 domainmapping = {}
 for color in colormapping:
     domainmapping[color] = []
-   
-for action in clicks:
-    domainmapping[action.domain].append(action.link)
-    action.color = colormapping[action.domain]
     
-plt.figure(figsize=(15,15))
+plt.figure(figsize=(10,10))
 plt.axis('off') 
 G = nx.MultiDiGraph()
-G.add_edges_from(nodes)
+#G.add_edges_from(nodes)
+G.add_edges_from(domains)
 values = [node.color for node in G.nodes()]
-labels = {clicks[node]:node for node in range(0, len(G.nodes()))}
+#labels = {clicks[node]:node for node in range(0, len(G.nodes()))}
+labels = {doms[node]:node for node in range(0, len(G.nodes()))}
 pos = nx.fruchterman_reingold_layout(G)
-gnodes = nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color = values, node_size=2000,)
+gnodes = nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color = values, node_size=2000)
 #nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
 #gedges = nx.draw_networkx_edges(G, pos, edgelist=nodes, arrows=True)
-for start, end, length in edges:
+for start, end in domainedges:
     # You can attach any attributes you want when adding the edge
-    G.add_edge(start, end, length=length)
-nx.draw_networkx_edges(G, pos, edgelist=edges, arrows=True, length=labels)
-nx.draw_networkx_labels(G,pos,labels,font_size=20)
+    G.add_edge(start, end)
+#nx.draw_networkx_edges(G, pos, edgelist=edges, arrows=True, length=labels)
+nx.draw_networkx_edges(G, pos, edgelist=domains, arrows=True)
+nx.draw_networkx_labels(G, pos, labels, font_size=20)
 
 plt.show()
 print ("total domains: " + str(len(domainmapping))) 
+
+
 
 
         
