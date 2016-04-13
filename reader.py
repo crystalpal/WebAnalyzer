69ed94653afa8acaf6258f2dd0efde7bd0e3a69c
crystalpal <joren.verspeelt@gmail.com>
2016-04-13 16:58:37 +0200

    basicpredication (only look-ahead)

--- reader.py
+++ reader.py
@@ -13,11 +13,11 @@ import time as tm
 
 
 file = 'C:/Users/Joren/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test.csv'
-file1 = 'F:/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test/march_13.csv'
+file1 = 'F:/Dropbox/Vince - Joren/Master Ai/Machine Learning - Project/Datasets/test/march_22.csv'
 
-colors = ["red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold", 
-          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"          
-          "red", "blue", "yellow", "green", "purple", "black", "orange", "pink", "gray", "brown", "white", "silver", "gold"]  
+colors = ["red", "blue", "yellow", "green", "purple", "white", "orange", "pink", "gray", "brown", "white", "silver", "gold", 
+          "red", "blue", "yellow", "green", "purple", "white", "orange", "pink", "gray", "brown", "white", "silver", "gold"          
+          "red", "blue", "yellow", "green", "purple", "white", "orange", "pink", "gray", "brown", "white", "silver", "gold"]  
     
 class Action(object):
     action =""
@@ -25,11 +25,12 @@ class Action(object):
     link = ""
     timestamp = ""
     color = ""
-    def __init__(self, action, domain, link, timeformat):
+    def __init__(self, action, domain, link, timeformat, color):
         self.action = action
         self.domain = domain
         self.link = link
         self.timestamp = timeformat
+        self.color = color
         
     def timedifference(self, action):
         time = tm.mktime(action.timestamp) - tm.mktime(self.timestamp)
@@ -38,9 +39,11 @@ class Action(object):
 class Domain(object):
     url = ""
     color = ""
+    urls = []
     def __init__(self, url, color):
         self.dom = url
         self.color = color
+        self.urls = []
     
 def line_prepender(filename, line):
     with open(filename, 'r+') as f:
@@ -60,8 +63,7 @@ data = csv.reader(f, delimiter=',')
 
 loads = []
 clicks = []
-doms = []
-d = []
+domains = {}
 
 
 iterrows = iter(data)
@@ -83,64 +85,73 @@ for row in iterrows:
                  tm_sec=second)#, tm_wday=3, tm_yday=335, tm_isdst=-1)
     print (datetime)
     '''
-    action = Action(act, domain, link, timeformat)
+    action = Action(act, domain, link, timeformat, colors[len(clicks)%9])
     if act == "load":
         loads.append(action)
     if act == "click":
         clicks.append(action)
-    if not action.domain in d:
-        dom = Domain(action.domain, colors[len(doms)])
-        doms.append(dom)
-        d.append(action.domain)
+        if not action.domain in domains.keys():
+            domains[action.domain] = Domain(action.domain, colors[len(domains.keys())%9])
    
-nodes = [] 
-domains = []
-domainedges = []
-maxtime = 0  
-for i in range(0, len(clicks)-1):
-    nodes.append((clicks[i], clicks[i+1])) 
-    time = clicks[i].timedifference(clicks[i+1])
-    if time > maxtime:
-        maxtime = time
-   
-for i in range(0, len(doms)-1):
-    domains.append((doms[i], doms[i+1]))
-     
-         
 edges = []
-for node in nodes:
-    time = node[0].timedifference(node[1])
-    edges.append((node[0], node[1], time/maxtime))
+domainedges = []
+linknodes = {}
+for i in range(0, len(clicks)):
+    c1 = clicks[i]
+    if not c1.link in linknodes:
+        linknodes[c1.link] = c1
+    if i+1 < len(clicks):
+        c2 = clicks[i+1]
+        time = c1.timedifference(c2)
+        edges.append((c1, c2)) 
+        dom1 = domains[c1.domain]
+        dom2 = domains[c2.domain]
+        if not dom1.dom == dom2.dom: 
+            domainedges.append((dom1, dom2))   
+    domains[c1.domain].urls.append(c1)
 
 
 colormapping = dict(zip(domains, colors))
-
-domainmapping = {}
-for color in colormapping:
-    domainmapping[color] = []
-    
+"""
 plt.figure(figsize=(10,10))
 plt.axis('off') 
 G = nx.MultiDiGraph()
-#G.add_edges_from(nodes)
-G.add_edges_from(domains)
+G.add_edges_from(domainedges)
 values = [node.color for node in G.nodes()]
-#labels = {clicks[node]:node for node in range(0, len(G.nodes()))}
-labels = {doms[node]:node for node in range(0, len(G.nodes()))}
+sizes = [len(node.urls)*1000 for node in G.nodes()]
+labels = {first:second.dom for (first, second) in domainedges}
 pos = nx.fruchterman_reingold_layout(G)
-gnodes = nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color = values, node_size=2000)
-#nx.draw_networkx_edges(G, pos, edgelist=red_edges, edge_color='r', arrows=True)
-#gedges = nx.draw_networkx_edges(G, pos, edgelist=nodes, arrows=True)
-for start, end in domainedges:
-    # You can attach any attributes you want when adding the edge
-    G.add_edge(start, end)
-#nx.draw_networkx_edges(G, pos, edgelist=edges, arrows=True, length=labels)
-nx.draw_networkx_edges(G, pos, edgelist=domains, arrows=True)
-nx.draw_networkx_labels(G, pos, labels, font_size=20)
+gnodes = nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), node_color = values, node_size=sizes)
+nx.draw_networkx_edges(G, pos, edgelist=domainedges, arrows=True)
+nx.draw_networkx_labels(G, pos, labels ,font_size=10)
 
+#plt.show()
+print ("total domains: " + str(len(colormapping))) 
+
+"""
+plt.figure(figsize=(10,10))
+plt.axis('off') 
+F = nx.MultiDiGraph()
+F.add_edges_from(edges)
+nodevalues = [node.color for node in F.nodes()]
+nodelabels = {clicks[node]:node for node in range(0, len(F.nodes()))}
+nodepos = nx.fruchterman_reingold_layout(F)
+gnodes = nx.draw_networkx_nodes(F, nodepos, cmap=plt.get_cmap('jet'), node_color = nodevalues, node_size=1500)
+nx.draw_networkx_edges(F, nodepos, edgelist=edges, arrows=True)
+nx.draw_networkx_labels(F, nodepos, nodelabels ,font_size=15)
 plt.show()
-print ("total domains: " + str(len(domainmapping))) 
 
+for start, end in edges:
+    F.add_edge(start, end)
+    #nx.draw_networkx_edges(G, pos, edgelist=edges, arrows=True, length=labels)
+
+proposedlink = clicks[5].link
+print(proposedlink)
+print("---")
+
+for n in F.neighbors(linknodes[proposedlink]):
+    for nn in F.neighbors(n):
+        print(nn.link)
 
 
 
